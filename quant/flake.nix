866cc31083f019a522feaf02d0a36e4ce958f0e6
Hugo Samayoa III <h3samayoa@gmail.com>
2025-05-06 20:25:04 -0700

    use llama.cpp nixpkgs

--- quant/flake.nix
+++ quant/flake.nix
@@ -1,14 +1,7 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    llama-cpp-src = {
-      url = "github:ggerganov/llama.cpp/764b85627b46f43d7ea801867cd1b6abef484574";
-      flake = false;
-    };
-  };
-
-  outputs = inputs @ { self, nixpkgs, llama-cpp-src, ... }: let 
+  outputs = inputs @ { self, nixpkgs, ... }: let
 
     forAllSystems = function:
       nixpkgs.lib.genAttrs [
@@ -18,46 +11,25 @@
       ] (system:
         function (import nixpkgs {
           inherit system;
+          config.allowUnfree = true;
         }));
-    
-    llamaPoetry = pkgs:
-      let
-        poetry-with-overrides = import ./overrides.nix { inherit pkgs; };
-      in
-        poetry-with-overrides.pkgs.buildPythonPackage {
-          pname = "llama-cpp-tools";
-          version = "unstable";
-          format = "pyproject";
-          src = llama-cpp-src;
-
-          nativeBuildInputs = with poetry-with-overrides.pkgs; [
-            poetry-core
-          ];
-
-          propagatedBuildInputs = with poetry-with-overrides.pkgs; [
-            torch
-            transformers
-            sentencepiece
-            huggingface-hub
-            numpy
-            protobuf
-          ];
-        };
-
   in {
     packages = forAllSystems (pkgs: {
-      llama-cpp = pkgs.callPackage ./package.nix {
-        src = llama-cpp-src;
-      };
+      default = pkgs.llama-cpp;
     });
 
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          python311
-          (llamaPoetry pkgs)
+          llama-cpp
           git
           cmake
+          python311
+          python311Packages.transformers
+          python311Packages.sentencepiece
+          python311Packages.huggingface-hub
+          python311Packages.torch
+          python311Packages.gguf
         ];
       };
     });
--- quant/package.nix
+++
@@ -1,8 +0,0 @@
-{ stdenv, cmake, git, curl, src }:
-
-stdenv.mkDerivation {
-  name = "llama-cpp-main";
-  inherit src;
-  nativeBuildInputs = [ cmake git curl ];
-
-}
